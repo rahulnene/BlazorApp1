e6a916377a0947a185ea22bd2a67be30b7566ae9
Rahul Nene <rahul.nene@live.com>
2023-12-01 01:21:21 +0530

    .

--- Data/ApiService.cs
+++ Data/ApiService.cs
@@ -2,53 +2,60 @@
 
 namespace BlazorApp1.Data
 {
-	public class ApiService
-	{
-		private readonly HttpClient _httpClient;
+    public class ApiService
+    {
+        private readonly HttpClient _httpClient;
 
-		public ApiService(HttpClient httpClient)
-		{
-			_httpClient = httpClient;
-		}
+        public ApiService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
 
-		public async Task<List<Character>?> GetAllCharacters()
-		{
-			try
-			{
-				return await _httpClient.GetFromJsonAsync<List<Character>>("https://localhost:7163/api/Character/GetAll");
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-				return [];
-			}
-		}
-		public async Task<Character?> GetCharacter(int id)
-		{
-			try
-			{
-				return await _httpClient.GetFromJsonAsync<Character>($"https://localhost:7163/api/Character/{id}");
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-				return null;
-			}
-		}
-		public async Task DeleteCharacter(int id)
-		{
+        public async Task<List<Character>?> GetAllCharacters()
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<List<Character>>("https://localhost:7163/api/Character/GetAll");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return [];
+            }
+        }
+        public async Task<Character?> GetCharacter(int id)
+        {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<Character>($"https://localhost:7163/api/Character/{id}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public async Task DeleteCharacter(int id)
+        {
 
-			var response = await _httpClient.DeleteAsync($"https://localhost:7163/api/Character/delete/{id}");
+            var response = await _httpClient.DeleteAsync($"https://localhost:7163/api/Character/delete/{id}");
 
-			if (!response.IsSuccessStatusCode)
-			{
-				throw new Exception($"Error: {response.StatusCode}");
-			}
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error: {response.StatusCode}");
+            }
+        }
+        public async Task AddCharacter(Character newCharacter)
+        {
+            var response = await _httpClient.PostAsJsonAsync<Character>("https://localhost:7163/api/Character/new", newCharacter);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error: {response.StatusCode}");
+            }
+        }
 
 
-		}
 
-
-
-	}
+    }
 }
